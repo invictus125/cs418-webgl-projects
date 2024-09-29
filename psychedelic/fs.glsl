c12b4e1f84cc9c41af70920e5a2837e746f1f75b
invictus125 <50851487+invictus125@users.noreply.github.com>
2024-09-29 15:42:15 -0500

    Initial implementation of shaders - two image options created

--- psychedelic/fs.glsl
+++ psychedelic/fs.glsl
@@ -1,3 +1,34 @@
+# version 300 es
+precision highp float;
+out vec4 color;
+uniform float seconds;
+in float x;
+in float y;
+float r_seed;
+float g_seed;
+float b_seed;
+float r;
+float g;
+float b;
+
 void main() {
-    
+    // Option 1: Swirls at edges, middle is stationary
+    // r_seed = ((x*x) + y*y + 2.0*x + 1.5*y) / 0.25*x*y;
+    // g_seed = ((0.5*x*x / 2.0*y) + y*y - 2.0*x + 1.5*y) / 0.5*y*y;
+    // b_seed = 2.0*x*x + (0.75*y*y / 2.0*x) + x - y;
+
+    // r = sin(r_seed * g_seed * seconds);
+    // g = cos(g_seed * g_seed * seconds);
+    // b = sin(b_seed * r_seed * seconds);
+
+    // Option 2: Dancing ovals
+    r_seed = (x*x + 0.7*x*y + y*y + 1.0);
+    g_seed = (x*x - 0.5*x*y + y*y + 2.0);
+    b_seed = (x*x + 0.5*x*y + y*y + 5.0);
+
+    r = sin(r_seed * seconds);
+    g = cos(g_seed * seconds);
+    b = sin(b_seed * seconds);
+
+    color = vec4(r, g, b, 1);
 }
--- psychedelic/vs.glsl
+++ psychedelic/vs.glsl
@@ -1,3 +1,15 @@
+#version 300 es
+uniform float seconds;
+out float x;
+out float y;
 void main() {
-    
+    // Vertex 0 - (-1, -1)
+    // Vertex 1 - (1, -1)
+    // Vertex 2 - (1, 1)
+    // Vertex 3 - (-1, -1)
+    // Vertex 4 - (-1, 1)
+    // Vertex 5 - (1, 1)
+    x = gl_VertexID == 0 || gl_VertexID == 3 || gl_VertexID == 4 ? -1.0 : 1.0;
+    y = gl_VertexID == 0 || gl_VertexID == 1 || gl_VertexID == 3 ? -1.0 : 1.0;
+    gl_Position = vec4(float(x), float(y), float(0), float(1));
 }
