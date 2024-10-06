22463099ba30e542a2200cc79ded1e09563d942a
invictus125 <50851487+invictus125@users.noreply.github.com>
2024-10-05 21:30:09 -0500

    Orbits: Sun scaled and rendered correctly

--- orbits/orbits.js
+++ orbits/orbits.js
@@ -60,68 +60,6 @@ var octahedron_data = {
 };
 
 /**
- * Sends per-vertex data to the GPU and connects it to a VS input
- * 
- * @param data    a 2D array of per-vertex data (e.g. [[x,y,z,w],[x,y,z,w],...])
- * @param loc     the layout location of the vertex shader's `in` attribute
- * 
- * @returns the ID of the buffer in GPU memory; useful for changing data later
- */
-function supplyDataBuffer(data, loc) {
-    const buf = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, buf)
-    const f32 = new Float32Array(data.flat())
-    gl.bufferData(gl.ARRAY_BUFFER, f32, gl.STATIC_DRAW)
-    
-    gl.vertexAttribPointer(loc, data[0].length, gl.FLOAT, false, 0, 0)
-    gl.enableVertexAttribArray(loc)
-    
-    return buf;
-}
-
-/**
- * Creates a Vertex Array Object and puts into it all of the data in the given
- * JSON structure, which should have the following form:
- * 
- * ````
- * {"triangles": a list of lists of indices of vertices
- * ,"attributes":
- *  [ a list of 1-, 2-, 3-, or 4-vectors, one per vertex to go in location 0
- *  , a list of 1-, 2-, 3-, or 4-vectors, one per vertex to go in location 1
- *  , ...
- *  ]
- * }
- * ````
- * 
- * @returns an object with four keys:
- *  - mode = the 1st argument for gl.drawElements
- *  - count = the 2nd argument for gl.drawElements
- *  - type = the 3rd argument for gl.drawElements
- *  - vao = the vertex array object for use with gl.bindVertexArray
- */
-function setupGeometry(geom) {
-    var triangleArray = gl.createVertexArray()
-    gl.bindVertexArray(triangleArray)
-
-    for(let i=0; i < geom.attributes.length; i++) {
-        let data = geom.attributes[i]
-        supplyDataBuffer(data, i)
-    }
-
-    var indices = new Uint16Array(geom.triangles.flat())
-    var indexBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
-
-    return {
-        mode: gl.TRIANGLES,
-        count: indices.length,
-        type: gl.UNSIGNED_SHORT,
-        vao: triangleArray
-    }
-}
-
-/**
  * Resizes the canvas to the largest square the screen can hold
  */
 function fillScreen() {
@@ -152,63 +90,20 @@ function tick(milliseconds) {
 }
 
 /**
- * Compiles two shaders, links them together, looks up their uniform locations,
- * and returns the result. Reports any shader errors to the console.
- *
- * @param {string} vs_source - the source code of the vertex shader
- * @param {string} fs_source - the source code of the fragment shader
- * @return {WebGLProgram} the compiled and linked program
- */
-function compile(vs_source, fs_source) {
-    const vs = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vs, vs_source);
-    gl.compileShader(vs);
-    if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
-        console.error(gl.getShaderInfoLog(vs));
-        throw Error("Vertex shader compilation failed");
-    }
-
-    const fs = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fs, fs_source);
-    gl.compileShader(fs);
-    if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
-        console.error(gl.getShaderInfoLog(fs));
-        throw Error("Fragment shader compilation failed");
-    }
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vs);
-    gl.attachShader(program, fs);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error(gl.getProgramInfoLog(program));
-        throw Error("Linking failed");
-    }
-    
-    const uniforms = {}
-    for(let i = 0; i < gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS); i += 1) {
-        let info = gl.getActiveUniform(program, i);
-        uniforms[info.name] = gl.getUniformLocation(program, info.name);
-    }
-    program.uniforms = uniforms;
-
-    return program;
-}
-
-/**
  * Creates matrices for each object and sends them into the GPU as uniforms.
  * Matrices will be based on the time elapsed in the animation.
  *
  * @param seconds The number of seconds into the animation we are
  */
 function processMatrices(seconds) {
-    var view = m4view([1,2,3], [0,0,0], [0,1,0]);
+    var view = m4view([1,1.2,8], [0,0,0], [0,1,0]);
     gl.uniformMatrix4fv(program.uniforms.perspective, false, perspective);
 
     // Handle the animation of the sun
-    var sunRotation = m4rotZ(seconds / 2.0);
+    var sunRotation = m4rotY(seconds * 2.0);
     var sunScale = m4scale(2.0, 2.0, 2.0);
-    gl.uniformMatrix4fv(program.uniforms.sunmatrix, false, m4mul(perspective, view, sunRotation, sunScale));
+    var sunMv = m4mul(view, sunRotation, sunScale);
+    gl.uniformMatrix4fv(program.uniforms.sunmv, false, sunMv);
 }
 
 /**
@@ -217,7 +112,6 @@ function processMatrices(seconds) {
  * @param {Number} seconds - the number of seconds since the animation began
  */
 function draw(seconds) {
-    gl.clear(gl.COLOR_BUFFER_BIT);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     gl.useProgram(program);
     gl.uniform1f(program.uniforms.seconds, seconds);
@@ -241,7 +135,7 @@ function draw(seconds) {
     gl.enable(gl.BLEND);
     const vs = await fetch('sunvs.glsl').then(res => res.text());
     const fs = await fetch('sunfs.glsl').then(res => res.text());
-    window.program = compile(vs, fs);
+    window.program = compileShader(vs, fs);
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -251,7 +145,7 @@ function draw(seconds) {
     fillScreen();
     window.addEventListener('resize', fillScreen);
 
-    tick(0);
+    requestAnimationFrame(tick);
 }
 
 // Entry point for the animation program
